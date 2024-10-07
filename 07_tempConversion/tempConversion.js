fddf185676934f9807ab5dbc4ceb0ef95a8c7ddb
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-07 22:06:38 +0300

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  return +((fTemp - 32) * 5/9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  return +(9/5 * cTemp + 32).toFixed(1);
 };
 
 // Do not edit below this line
