02e013b5b063abaac369722cf6749aaec8524867
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-07 21:26:50 +0300

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	if (
+		!Number.isInteger(num1) ||
+		!Number.isInteger(num2)||
+		num1 < 0 ||
+		num2 < 0
+	) {
+		return "ERROR";
+	}
+	const lower = Math.min(num1, num2);
+	const upper = Math.max(num1, num2);
 
+	let sumResult = 0;
+
+	for (let i = lower; i <= upper; i++) {
+		sumResult += i;
+	}
+
+	return sumResult;
 };
 
 // Do not edit below this line
