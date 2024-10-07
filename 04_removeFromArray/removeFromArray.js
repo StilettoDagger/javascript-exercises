e14f8f0a8da466d593069e0ee5b244b7082c45d7
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-07 21:13:10 +0300

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const newArr = arr.filter(element => !args.includes(element));
+    return newArr;
 };
 
 // Do not edit below this line
