96bc9ad14f44a825ae56e852ae1808fb6689042b
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-07 20:45:11 +0300

    Solve exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const strLetters = str.split("");
+    const strLen = strLetters.length;
+    let resultStr = "";
 
+    for (let i = strLen - 1; i >= 0; i--){
+        resultStr += strLetters[i];
+    }
+    return resultStr;
 };
 
 // Do not edit below this line
