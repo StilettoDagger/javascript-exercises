54201dc2ad689b64feb9542723c62f49b013d453
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-15 13:56:58 +0300

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+    // Get a new array of objects including people's ages
+    const agedPeople = people.map(person => {
+        if ("yearOfDeath" in person)
+        {
+            person["age"] = person.yearOfDeath - person.yearOfBirth;
+        }
+        else
+        {
+            person["age"] = (new Date().getFullYear()) - person.yearOfBirth;
+        }        
+
+        return person;
+    })
+
+    // Find the oldest person from the new array of people
+    const oldestPerson = agedPeople.reduce((oldest, person) => {
+        if (person.age > oldest.age)
+        {
+            oldest = person;
+        }
+        return oldest;
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
