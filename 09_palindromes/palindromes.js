c564497e3ffaba8ffded0877680536eeb9afc4d1
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-10-15 12:56:29 +0300

    Solve exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let reversedStr = "";
 
+    let charTest = /[a-zA-Z0-9]/g;
+
+    const characters = str.match(charTest);
+    
+    for (let i = characters.length - 1; i >= 0; i--)
+        {
+            reversedStr += characters[i];
+    }
+
+    const alphaStr = characters.join("")
+
+    return reversedStr.toLowerCase() == alphaStr.toLowerCase();
 };
 
 // Do not edit below this line
